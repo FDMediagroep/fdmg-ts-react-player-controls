72856c80d689a7e9de189c0ea291186960edac9d
Willem Liu <willem_liu@hotmail.com>
2017-12-20 12:32:02 +0100

    Added default styles, images and a demo page to show the component.

---
+++ demo/src/main.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import '../../scss/PlayerControls.scss';
+import '../../img/playercontrols/next-button.svg';
+import '../../img/playercontrols/pause-button.svg';
+import '../../img/playercontrols/play-button.svg';
+import '../../img/playercontrols/previous-button.svg';
+import PlayerControls from '../../src/PlayerControls';
+
+// Render react components.
+ReactDOM.render(
+    <PlayerControls
+        playPauseButtonImage='img/play-button.svg'
+        nextButtonImage='img/next-button.svg'
+        previousButtonImage='img/previous-button.svg'
+        className='player-controls'
+        showPreviousButton={true}
+        showPlayPauseButton={true}
+        showNextButton={true}
+        onPrevious={() => { alert('previous') }}
+        onPlayPause={() => { alert('play/pause') }}
+        onNext={() => {alert('next') }}
+    />,
+    document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,108 @@
+/**
+ * This Webpack config is used for bundling files for the demo.
+ */
+
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
+
+console.info('process.env.NODE_ENV', JSON.stringify(process.env.NODE_ENV));
+
+const extractSass = new ExtractTextPlugin({
+    filename    : "../css/[name].css",
+    disable     : process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+    devtool: "source-map",
+    entry: {
+        main: [
+            "./demo/src/main.tsx?"
+        ]
+    },
+    output: {
+        path       : path.join(__dirname, '/demo/src'),
+        filename   : '[name].js',
+        publicPath : './demo/src'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        alias: {
+            'react': 'preact-compat',
+            'react-dom': 'preact-compat'
+        }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+            }
+        }),
+        extractSass,
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+              warnings: false,
+              screw_ie8: true,
+              conditionals: true,
+              unused: true,
+              comparisons: true,
+              sequences: true,
+              dead_code: true,
+              evaluate: true,
+              if_return: true,
+              join_vars: true
+            },
+            output: {
+              comments: false
+            }
+        })
+    ],
+    module: {
+        rules: [
+            // all files with a `.ts` or `.tsx` extension will be handled by a TypeScript loader
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
+                options: {
+                    configFileName: './demo/tsconfig.json'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [{
+                        loader: "css-loader",
+                        options: {
+                          sourceMap: true
+                        }
+                    }, {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }],
+                    // use style-loader in development
+                    fallback: "style-loader"
+                })
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            publicPath: 'img/',
+                            outputPath: '../img/'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
